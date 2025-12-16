c72df5d7559121c0fb23838c910212ceeee36014
codebyte0 <codebyte.wd@gmail.com>
2025-12-16 23:04:38 +0500

    experience section

---
+++ src/components/ExperienceSection.tsx
@@ -0,0 +1,11 @@
+
+const ExperienceSection = () => {
+  return (
+    <div>
+        <h2>Experience Section</h2>
+        <p>This is where the experience details will go.</p>
+    </div>
+  )
+}
+
+export default ExperienceSection
